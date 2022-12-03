d2b38dd6c0b3c2e1aaac2a9633e8732c866a46cb
Maryam <mryammansour411@gmail.com>
2022-12-03 19:12:20 +0200

    Including Decorator in Discounts Interface

--- Software project/src/Services/MainClass.java
+++ Software project/src/Services/MainClass.java
@@ -1,9 +1,13 @@
 package Services;
 
+import Mobile.MobileWE;
+import Payment.Payment;
+import Payment.PayByCash;
 public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("hello world maryam");
+
 
 	}
 
